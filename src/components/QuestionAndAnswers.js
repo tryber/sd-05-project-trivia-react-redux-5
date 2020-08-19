095c4b6e65ba2748c9adc68084d8674a331e83e2
pedrohcalado <pedrocalado22@gmail.com>
2020-08-19 18:32:10 -0300

    adiciona random answers

--- src/components/QuestionAndAnswers.js
+++ src/components/QuestionAndAnswers.js
@@ -14,6 +14,36 @@ class QuestionAndAnswers extends React.Component {
     this.setState({ index: this.state.index + 1 });
   }
 
+  randomAnswers(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      const a = array[i];
+      array[i] = array[j];
+      array[j] = a;
+    }
+    return array;
+  }
+
+  answers(questions) {
+    const { correct_answer, incorrect_answers } = questions;
+    const correct = {
+      answer: correct_answer,
+      isCorrect: true,
+      'data-testid': 'correct-answer',
+    };
+    const incorrect = incorrect_answers.map((answer, i) => ({
+      answer,
+      isCorrect: false,
+      'data-testid': `wrong-answer-${i}`,
+    }));
+    
+    const answers = [...incorrect, correct]
+
+    const randomAnswers = [...answers];
+    
+    return this.randomAnswers(randomAnswers);
+  }
+
   render() {
     const { questions } = this.props;
     const { index } = this.state;
@@ -21,7 +51,14 @@ class QuestionAndAnswers extends React.Component {
       <div>
         {questions[index] &&
           <div>
-            {questions[index].category}
+            <p data-testid="question-category">{questions[index].category}</p>
+            <p data-testid="question-text">{questions[index].question}</p>
+            {
+            this.answers(questions[index]).map((answer) =>
+            (<button data-testid={answer["data-testid"]}>{answer.answer}</button>)
+            )
+            /* <button data-testid="correct-answer">{questions[index].correct_answer}</button>
+            {questions[index].incorrect_answers.map((answer, i) => <button data-testid={`wrong-answer-${i}`}>{answer}</button>)} */}
           </div>
         }
         <button onClick={() => this.handleClick()}>Próxima</button>
