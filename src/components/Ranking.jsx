7bdeb59c69bdc80e8d3f0bf98b9d586f12db8685
pedrohcalado <pedrocalado22@gmail.com>
2020-08-21 15:16:05 -0300

    adiciona botao go home no ranking

--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const Ranking = () => <div data-testid="ranking-title">Ranking</div>;
+const Ranking = () => {
+  return (
+    <div>
+      <div data-testid="ranking-title">Ranking</div>
+      <button data-testid="btn-go-home">Go home</button>
+    </div>
+  );
+};
 
 export default Ranking;
