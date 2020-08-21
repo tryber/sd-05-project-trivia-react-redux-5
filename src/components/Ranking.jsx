bc32aad891492d317e87d64768c07e94795bd588
pedrohcalado <pedrocalado22@gmail.com>
2020-08-21 15:19:58 -0300

    cc

--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Ranking = () => (
-    <div>
-      <div data-testid="ranking-title">Ranking</div>
-      <button data-testid="btn-go-home">Go home</button>
-    </div>
+  <div>
+    <div data-testid="ranking-title">Ranking</div>
+    <button data-testid="btn-go-home">Go home</button>
+  </div>
 );
 
 export default Ranking;
