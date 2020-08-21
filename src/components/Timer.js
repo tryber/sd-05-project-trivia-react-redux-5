15ada9a140700a72f1b10a675ffef553368148a3
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-08-21 09:27:56 -0300

    cc

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 // import { interval } from './QuestionAndAnswers';
 import { disableButton } from '../action';
-import PropTypes from 'prop-types';
 
 function Timer(props) {
   if (props.timer < 1 && props.timer !== null) {
